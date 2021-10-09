c74d517a0a7180baad497105f57c80814222d051
andififian <70189494+andififian@users.noreply.github.com>
2021-10-10 00:09:39 +0700

    problem-6

---
+++ Problem-6/Assets/Scripts/BallFollowCursor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFollowCursor : MonoBehaviour
+{
+    private Rigidbody2D ballRigidbody;
+    public void Update(){
+        Vector2 PosCursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(PosCursor.x, PosCursor.y);
+    }
+    private void Start(){
+        ballRigidbody = GetComponent<Rigidbody2D>();
+    }
+}
