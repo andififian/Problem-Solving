7cb91fad65cf2d2f8e6dbd19ee80bc196dcab024
andififian <70189494+andififian@users.noreply.github.com>
2021-10-10 02:19:47 +0700

    problem-7

---
+++ Problem-7/Assets/Scripts/ObstaclesSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesSpawner : MonoBehaviour
+{
+	public Transform[] Pos;
+	public GameObject Obj;
+
+	public Transform Loc;
+
+	public bool ToSpawn = true;
+
+	// Start is called before the first frame update
+	void Update()
+	{
+		Loc = Pos[Random.Range(0, Pos.Length)];
+
+		if (ToSpawn == true)
+		{
+			Instantiate(Obj, Loc);
+			ToSpawn = false;
+			StartCoroutine(ToSpawnTrue());
+		}
+	}
+
+	IEnumerator ToSpawnTrue()
+	{
+		yield return new WaitForSeconds(0.75f);
+		ToSpawn = true;
+	}
+}
