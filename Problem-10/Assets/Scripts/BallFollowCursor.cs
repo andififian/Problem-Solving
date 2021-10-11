7a6fc34bedf55ee36762e628b1d35d1cc8776639
andififian <70189494+andififian@users.noreply.github.com>
2021-10-11 10:17:35 +0700

    problem-10

---
+++ Problem-10/Assets/Scripts/BallFollowCursor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFollowCursor : MonoBehaviour
+{
+    private Rigidbody2D ballRigidbody;
+    public Camera mainCamera;
+    [Header("Ball Movement")]
+    public Vector2 ballpos = new Vector2(100, 100);
+    public float speed;
+    public ScoreController scoreController;
+
+     private void Start(){
+        ballRigidbody = GetComponent<Rigidbody2D>();
+    }
+    public void Update(){
+        Vector3 target = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+        Vector3 moveAngle = target - transform.position;
+
+        if (Vector3.Distance(target, transform.position) > 0.5)
+            transform.Translate(moveAngle.normalized * speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Box")
+        {
+            scoreController.IncreaseCurrentScore(1);
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Problem-10/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Button but1;
+    [SerializeField] private Button but2;
+    [SerializeField] private Button but3;
+    [SerializeField] private Button but4;
+    [SerializeField] private Button but5;
+    [SerializeField] private Button but6;
+    [SerializeField] private Button but7;
+    [SerializeField] private Button but8;
+    [SerializeField] private Button but9;
+
+    private void Start()
+    {
+        but1.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        but2.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(2);
+        });
+
+        but3.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(3);
+        });
+
+        but4.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(4);
+        });
+
+        but5.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(5);
+        });
+
+        but6.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(6);
+        });
+
+        but7.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(7);
+        });
+
+        but8.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(8);
+        });
+
+        but9.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(9);
+        });
+    }
+}
